003638ef0c3a533ab09d9d9a1b03bda8b52c94cb
vgzowski <vovamorozovfool@gmail.com>
2023-12-29 14:09:16 +0100

    Added functionality, minor fixes, still only cpp file

--- FFT/testing.cpp
+++ FFT/testing.cpp
@@ -7,8 +7,8 @@ signed main() {
 	FFT::init();
 
 	{
-		vector <int> a{12334351, 782452};
-		vector <int> b{85412786, 3458855, 33};
+		vector <int> a{123341, 7452};
+		vector <int> b{854126, 38855, 33};
 
 		auto c = FFT::multiply_mod( a, b, (int)998244353 );
 
@@ -30,7 +30,7 @@ signed main() {
 		vector <double> a{1.1, 2.3};
 		vector <double> b{2.3, 4.1, 4.3};
 
-		vector <long long> c = FFT::multiply(a, b);
+		vector <long long> c = FFT::multiply<double>(a, b);
 
 		for (auto &i : c) cout << i << " ";
 		cout << '\n';
