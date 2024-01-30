7280aac15a4e7634e22ec231b774211b13a0c1e9
vgzowski <vovmorozov2@gmail.com>
2024-01-30 14:28:06 +0100

    Add size policy

---
+++ policies/Config/SizePolicy.h
@@ -0,0 +1,12 @@
+#pragma once
+#include <string>
+
+struct FixedSizePolicy {
+	const std::string str = "Fixed size";
+};
+struct OneElementPolicy {
+	const std::string str = "One element";
+};
+struct DynamicSizePolicy {
+	const std::string str = "Dynamic size";
+};
