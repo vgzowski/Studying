be884234ae782304f41b33a12395687067f2a92f
vgzowski <vovmorozov2@gmail.com>
2024-02-01 18:02:08 +0100

    Add testing

--- oop/Factory/FactoryMethod.cpp
+++ oop/Factory/FactoryMethod.cpp
@@ -97,4 +97,31 @@ private:
 };
 
 int main() {
+	SPAM_System spammer( std::make_unique < EmailCreator > () );
+
+	spammer.send_spam( "IvanIvanovich@pochta.ru", "Subscribe to vgzowski on github" );
+
+	spammer.send_spam( "IvanovIvan228@pochta.ru", "Subscribe to vgzowski on github" );
+
+	spammer.changeFactory( std::make_unique < PushCreator > () );
+
+	spammer.send_spam( "IvanIvanovich phone", "Subscribe to vgzowski on github" );
+
+	spammer.send_spam( "IvanIvanovich smart watch", "Subscribe to vgzowski on github" );
+
+	spammer.changeFactory( std::make_unique < SMSCreator > () );
+
+	spammer.send_spam( "+1-23-34-5678-9-10", "Subscribe to vgzowski on github" );
+
+	spammer.changeFactory( std::make_unique < PushCreator > () );
+
+	spammer.send_spam( "IvanIvanovich TV", "Subscribe to vgzowski on github" );
+
+	spammer.changeFactory( std::make_unique < EmailCreator > () );
+
+	spammer.send_spam( "IvanIvanovichFakeEmail@pochta.ru", "Subscribe to vgzowski on github" );
+
+	spammer.changeFactory( std::make_unique < SMSCreator > () );
+
+	spammer.send_spam( "+12-34-567-89", "Subscribe to vgzowski on github" );
 }
