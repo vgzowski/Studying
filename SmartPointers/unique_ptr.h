cb292b425d4c879dc966f5de11907621e81ab68b
vgzowski <vovmorozov2@gmail.com>
2024-02-12 16:27:26 +0100

    Add debug

--- SmartPointers/unique_ptr.h
+++ SmartPointers/unique_ptr.h
@@ -1,23 +1,31 @@
 #pragma once
+#include <iostream>
 
 template <class T>
 class uPtr {
 public:
-	uPtr(T* ptr) : ptr_(ptr) {}
+	uPtr(T* ptr) : ptr_(ptr) {
+		std::cout << "Default constructor" << std::endl;
+	}
 
 	uPtr(const uPtr &other) = delete;
 	uPtr& operator = (const uPtr &other) = delete;
 
 	uPtr& operator = (uPtr&& other) noexcept {
-		ptr_ = other.ptr_;
-		other.ptr_ = nullptr;
+		std::cout << "Move assignment operator" << std::endl;
+		if (this != &other) {
+			ptr_ = other.ptr_;
+			other.ptr_ = nullptr;
+		}
 		return *this;
 	}
 	uPtr(uPtr&& other) noexcept : ptr_(other.ptr_) {
+		std::cout << "Move assignment constructor" << std::endl;
 		other.ptr_ = nullptr;
 	}
 
 	~uPtr() {
+		std::cout << "Destruction" << std::endl;
 		delete ptr_;
 	}
 
