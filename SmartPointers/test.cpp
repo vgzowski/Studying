81ca69e1681313d4196709bc361ad39f5ad4d1a4
vgzowski <vovmorozov2@gmail.com>
2024-02-12 16:27:44 +0100

    Adjust testing for unique pointer

--- SmartPointers/test.cpp
+++ SmartPointers/test.cpp
@@ -20,24 +20,78 @@ struct My {
 
 int main() {
 { // unique pointer
+std::cout << "line 1:" << std::endl;
 	uPtr<int> a(new int(5));
-	std::cout << *a << std::endl;
+std::cout << std::endl << "-----------" << std::endl;
 
+std::cout << "line 2:" << std::endl;
+	std::cout << *a << std::endl;
+std::cout << std::endl << "-----------" << std::endl;
+
+std::cout << "line 3:" << std::endl;
+	a = std::move(a);
+std::cout << std::endl << "-----------" << std::endl;
+
+std::cout << "line 4:" << std::endl;
+	std::cout << *a << std::endl;
+std::cout << std::endl << "-----------" << std::endl;
+
+std::cout << "line 5:" << std::endl;
+	a = uPtr<int>(std::move(a));
+std::cout << std::endl << "-----------" << std::endl;
+
+std::cout << "line 6:" << std::endl;
+	std::cout << *a << std::endl;
+std::cout << std::endl << "-----------" << std::endl;
+
+std::cout << "line 7:" << std::endl;
 	uPtr<int> b = std::move(a);
+std::cout << std::endl << "-----------" << std::endl;
+
+std::cout << "line 8:" << std::endl;
 	std::cout << *b << std::endl;
+std::cout << std::endl << "-----------" << std::endl;
 //	std::cout << *a << std::endl; RE
 
 //	a = b; CE
+
+std::cout << "line 9:" << std::endl;
 	uPtr<int> c = std::move(b);
+std::cout << std::endl << "-----------" << std::endl;
+
+std::cout << "line 10:" << std::endl;
 	std::cout << *c << std::endl;
+std::cout << std::endl << "-----------" << std::endl;
 
+std::cout << "line 11:" << std::endl;
 	uPtr<My> d(new My());
-	d->foo();
+std::cout << std::endl << "-----------" << std::endl;
 
+std::cout << "line 12:" << std::endl;
+	d->foo();
+std::cout << std::endl << "-----------" << std::endl;
+
+std::cout << "line 13:" << std::endl;
 	uPtr<My> e = std::move(d);
+std::cout << std::endl << "-----------" << std::endl;
+
+std::cout << "line 14:" << std::endl;
 	e->foo();
+std::cout << std::endl << "-----------" << std::endl;
+
+std::cout << "line 15:" << std::endl;
+	d = std::move(e);
+std::cout << std::endl << "-----------" << std::endl;
+
+std::cout << "line 16:" << std::endl;
+	d->foo();
+std::cout << std::endl << "-----------" << std::endl;
 }
-{ // sahred pointer
+
+	std::cout << std::endl << "---------------------------------------------" << std::endl;
+	return 0;
+
+{ // shared pointer
 	sPtr<int> a(new int(7));
 	std::cout << *a << std::endl;
 
@@ -51,6 +105,12 @@ int main() {
 	a = b;
 
 	std::cout << "count b is: " << b.use_count() << std::endl;
+	std::cout << "count a is: " << a.use_count() << std::endl;
+
+	{
+		sPtr<int> c = a;
+		std::cout << "count a is: " << a.use_count() << std::endl;
+	}
 	std::cout << "count a is: " << a.use_count() << std::endl;
 
 	a = sPtr<int>(new int(4));
