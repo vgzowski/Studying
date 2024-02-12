0f6995fdd438017fd55928bda2541bad05ac1b59
vgzowski <vovmorozov2@gmail.com>
2024-02-12 15:55:07 +0100

    Add testing for shared pointer

--- SmartPointers/test.cpp
+++ SmartPointers/test.cpp
@@ -1,5 +1,16 @@
+#define TESTING
+
 #include <iostream>
+
+#ifdef TESTING
 #include "unique_ptr.h"
+#include "shared_ptr.h"
+#else
+#include <memory>
+#include <utility>
+#define uPtr std::unique_ptr
+#define sPtr std::shared_ptr
+#endif
 
 struct My {
 	void foo() {
@@ -8,7 +19,8 @@ struct My {
 };
 
 int main() {
-	uPtr<int> a = new int(5);
+{ // unique pointer
+	uPtr<int> a(new int(5));
 	std::cout << *a << std::endl;
 
 	uPtr<int> b = std::move(a);
@@ -19,9 +31,38 @@ int main() {
 	uPtr<int> c = std::move(b);
 	std::cout << *c << std::endl;
 
-	uPtr<My> d = new My();
+	uPtr<My> d(new My());
 	d->foo();
 
 	uPtr<My> e = std::move(d);
 	e->foo();
+}
+{ // sahred pointer
+	sPtr<int> a(new int(7));
+	std::cout << *a << std::endl;
+
+	sPtr<int> b = std::move(a);
+	std::cout << *b << std::endl;
+	
+//	std::cout << *a << std::endl; RE
+
+	std::cout << "count b is: " << b.use_count() << std::endl;
+
+	a = b;
+
+	std::cout << "count b is: " << b.use_count() << std::endl;
+	std::cout << "count a is: " << a.use_count() << std::endl;
+
+	a = sPtr<int>(new int(4));
+
+	sPtr<int> c = std::move(b);
+	std::cout << *c << std::endl;
+	std::cout << *a << std::endl;
+
+	sPtr<My> d(new My());
+	d->foo();
+
+	sPtr<My> e = std::move(d);
+	e->foo();
+}
 }
