06f4d1ea3a40210e74e5c72fb0cb7e1d0385f385
vgzowski <vovmorozov2@gmail.com>
2024-02-12 15:42:45 +0100

    Add shared_ptr implementation

---
+++ SmartPointers/shared_ptr.h
@@ -0,0 +1,41 @@
+#pragma once
+
+template <class T>
+class sPtr {
+public:
+	sPtr(T* ptr) : refCount_(new int(1)), ptr_(ptr) {}
+	sPtr(const sPtr &other) {
+		ptr_ = other.ptr_;
+		refCount_ = other.refCount_;
+		if (other.ptr_ != nullptr) {
+			++(*refCount_);
+		}
+	}
+	sPtr& operator = (const sPtr &other) {
+		if (this != &other) {
+			ptr_ = other.ptr_;
+			refCount_ = other.refCount_;
+			if (other.ptr_ != nullptr) {
+				++(*refCount_);
+			}
+		}
+		return *this;
+	}
+	sPtr(sPtr&& other) noexcept : ptr_(other.ptr_), refCount_(other.refCount_) {
+		other.ptr_ = other.refCount_ = nullptr;
+	}
+	sPtr& operator = (sPtr&& other) noexcept {
+		ptr_ = other.ptr_;
+		refCount_ = other.refCount_;
+		other.ptr_ = other.refCount_ = nullptr;
+		return *this;
+	}
+	~sPtr() {
+		if (refCount_ && !(*refCount)) {
+			delete ptr_;
+		}
+	}
+private:
+	int *refCount_;
+	T* ptr_;
+};
